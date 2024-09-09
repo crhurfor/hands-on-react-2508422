2152051ac86f5b8d30e58dca90e5735eb7751bdb
Calvin Hurford <72561729+crhurfor@users.noreply.github.com>
2024-09-09 15:50:37 +0000

    Added components

---
+++ src/components/Support.jsx
@@ -0,0 +1,12 @@
+import { useState} from 'react';
+
+function Support() {
+  const [count, setCount] = useState(0);
+  return (
+    <button className="outline" onClick={() => setCount(count +1)}>
+      {count === 0 ? "Click to Support": `Supported ${count} times`}
+    </button>
+  )
+}
+
+export default Support;
